d395fc932d37b11d5c83d8e40d18a5e5e6cfa294
Michael Wang <z5421148@ad.unsw.edu.au>
2023-07-14 15:07:04 +1000

    rewrote the zombie-toast to remove some currently unused code and possibly unneeded code (that I overthought when I was writing this).

--- src/main/java/dungeonmania/entities/enemies/ZombieToast.java
+++ src/main/java/dungeonmania/entities/enemies/ZombieToast.java
@@ -3,8 +3,8 @@ package dungeonmania.entities.enemies;
 import java.util.List;
 
 import dungeonmania.Game;
-import dungeonmania.entities.Boulder;
-import dungeonmania.entities.Entity;
+// import dungeonmania.entities.Boulder;
+// import dungeonmania.entities.Entity;
 import dungeonmania.entities.collectables.potions.InvisibilityPotion;
 import dungeonmania.entities.enemies.movementStrategy.DefaultRandomMovementStrategy;
 import dungeonmania.entities.enemies.movementStrategy.InvisibilityMovementStrategy;
@@ -24,19 +24,19 @@ public class ZombieToast extends Enemy {
         forward = true;
     }
 
-    private void updateNextPosition() {
-        if (forward) {
-            nextPositionElement++;
-            if (nextPositionElement == movementTrajectory.size()) {
-                nextPositionElement = 0;
-            }
-        } else {
-            nextPositionElement--;
-            if (nextPositionElement == -1) {
-                nextPositionElement = movementTrajectory.size() - 1;
-            }
-        }
-    }
+    // private void updateNextPosition() {
+    //     if (forward) {
+    //         nextPositionElement++;
+    //         if (nextPositionElement == movementTrajectory.size()) {
+    //             nextPositionElement = 0;
+    //         }
+    //     } else {
+    //         nextPositionElement--;
+    //         if (nextPositionElement == -1) {
+    //             nextPositionElement = movementTrajectory.size() - 1;
+    //         }
+    //     }
+    // }
 
     @Override
     public void move(Game game) {
@@ -46,19 +46,20 @@ public class ZombieToast extends Enemy {
             setNextPositionStrategy(new DefaultRandomMovementStrategy());
         }
         Position nextPos = getNextPosition(game);
-        List<Entity> entities = game.getMap().getEntities(nextPos);
-        if (entities != null && entities.size() > 0 && entities.stream().anyMatch(e -> e instanceof Boulder)) {
-            forward = !forward;
-            updateNextPosition();
-            updateNextPosition();
-        }
-        nextPos = movementTrajectory.get(nextPositionElement);
-        entities = game.getMap().getEntities(nextPos);
-        if (entities == null || entities.size() == 0
-                || entities.stream().allMatch(e -> e.canMoveOnto(game.getMap(), this))) {
-            game.getMap().moveTo(this, nextPos);
-            updateNextPosition();
-        }
+        game.getMap().moveTo(this, nextPos);
+        // List<Entity> entities = game.getMap().getEntities(nextPos);
+        // if (entities != null && entities.size() > 0 && entities.stream().anyMatch(e -> e instanceof Boulder)) {
+        //     forward = !forward;
+        //     updateNextPosition();
+        //     updateNextPosition();
+        // }
+        // nextPos = movementTrajectory.get(nextPositionElement);
+        // entities = game.getMap().getEntities(nextPos);
+        // if (entities == null || entities.size() == 0
+        //         || entities.stream().allMatch(e -> e.canMoveOnto(game.getMap(), this))) {
+        //     game.getMap().moveTo(this, nextPos);
+        //     updateNextPosition();
+        // }
     }
 
 }
