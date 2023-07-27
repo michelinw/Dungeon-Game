b0d503d77695e6aab5f64475d72495b05c0909c1
Rohan Agarwal <rohanAgarwal@student.unsw.edu.au>
2023-07-28 03:09:47 +1000

    refactored inventory.java

--- src/main/java/dungeonmania/entities/inventory/Inventory.java
+++ src/main/java/dungeonmania/entities/inventory/Inventory.java
@@ -34,22 +34,18 @@ public class Inventory {
 
         int wood = count(Wood.class);
         int arrows = count(Arrow.class);
-        int treasure = count(Treasure.class);
-        int keys = count(Key.class);
-        int sword = count(Sword.class);
-        int sunStone = count(SunStone.class);
         List<String> result = new ArrayList<>();
 
         if (wood >= 1 && arrows >= 3) {
             result.add("bow");
         }
-        if (wood >= 2 && (treasure >= 1 || keys >= 1)) {
+        if (shieldCanBeBuilt()) {
             result.add("shield");
         }
-        if (sword >= 1 && sunStone >= 1) {
+        if (midnightArmourCanBeBuilt()) {
             result.add("midnight_armour");
         }
-        if ((wood >= 1 || arrows >= 2) && (keys >= 1 || treasure >= 1) && sunStone >= 1) {
+        if (sceptreCanBeBuilt()) {
             result.add("sceptre");
         }
         return result;
@@ -64,19 +60,16 @@ public class Inventory {
         List<Sword> sword = getEntities(Sword.class);
         List<SunStone> sunStone = getEntities(SunStone.class);
 
-        if (wood.size() >= 1 && arrows.size() >= 3 && !forceShield) {
+        if (bowCanBeBuilt(forceShield)) {
             if (remove) {
-                items.remove(wood.get(0));
-                items.remove(arrows.get(0));
-                items.remove(arrows.get(1));
-                items.remove(arrows.get(2));
+                removeItems(wood, 1);
+                removeItems(arrows, 3);
             }
             return factory.buildBow();
 
-        } else if (wood.size() >= 2 && (treasure.size() >= 1 || keys.size() >= 1 || sunStone.size() >= 1)) {
+        } else if (shieldCanBeBuilt()) {
             if (remove) {
-                items.remove(wood.get(0));
-                items.remove(wood.get(1));
+                removeItems(wood, 2);
                 if (treasure.size() >= 1) {
                     items.remove(treasure.get(0));
                 } else {
@@ -85,47 +78,80 @@ public class Inventory {
             }
             return factory.buildShield();
 
-        } else if (sword.size() >= 1 && sunStone.size() >= 1) {
+        } else if (midnightArmourCanBeBuilt()) {
             if (remove) {
                 items.remove(sword.get(0));
                 items.remove(sunStone.get(0));
             }
             return factory.buildMidnightArmour();
 
-        } else if ((wood.size() >= 1 || arrows.size() >= 2) && (keys.size() >= 1 || treasure.size() >= 1)
-                && sunStone.size() >= 1) {
+        } else if (sceptreCanBeBuilt()) {
             if (remove) {
-                if (wood.size() >= 1 && sunStone.size() >= 2) {
-                    items.remove(sunStone.get(1));
-                    items.remove(sunStone.get(0));
-                } else if (arrows.size() >= 2 && sunStone.size() >= 2) {
-                    items.remove(arrows.get(1));
-                    items.remove(arrows.get(0));
-                    items.remove(sunStone.get(1));
-                    items.remove(sunStone.get(0));
-                } else if (wood.size() >= 1 && keys.size() >= 1 && sunStone.size() >= 1) {
-                    items.remove(wood.get(0));
-                    items.remove(keys.get(0));
-                    items.remove(sunStone.get(0));
-                } else if (wood.size() >= 1 && treasure.size() >= 1 && sunStone.size() >= 1) {
-                    items.remove(wood.get(0));
-                    items.remove(treasure.get(0));
-                    items.remove(sunStone.get(0));
-                } else if (arrows.size() >= 2 && keys.size() >= 1 && sunStone.size() >= 1) {
-                    items.remove(arrows.get(0));
-                    items.remove(arrows.get(1));
-                    items.remove(keys.get(0));
-                    items.remove(sunStone.get(0));
-                } else if (arrows.size() >= 2 && treasure.size() >= 1 && sunStone.size() >= 1) {
-                    items.remove(arrows.get(0));
-                    items.remove(arrows.get(1));
-                    items.remove(treasure.get(0));
-                    items.remove(sunStone.get(0));
+                if (wood.size() < 1) {
+                    removeItems(arrows, 2);
+                } else {
+                    removeItems(wood, 1);
                 }
+                if (keys.size() >= 1) {
+                    removeItems(keys, 1);
+                } else if (treasure.size() - sunStone.size() >= 1) {
+                    removeItems(treasure, 1);
+                }
+                removeItems(sunStone, 1);
             }
             return factory.buildSceptre();
         }
         return null;
+    }
+
+    private void removeItems(List<? extends InventoryItem> itemList, int count) {
+        for (int i = 0; i < count; i++) {
+            items.remove(itemList.get(i));
+        }
+    }
+
+    public boolean shieldCanBeBuilt() {
+        List<Wood> wood = getEntities(Wood.class);
+        List<Treasure> treasure = getEntities(Treasure.class);
+        List<Key> keys = getEntities(Key.class);
+        List<SunStone> sunStone = getEntities(SunStone.class);
+
+        if (wood.size() >= 2 && (treasure.size() >= 1 || keys.size() >= 1 || sunStone.size() >= 1)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean bowCanBeBuilt(boolean forceShield) {
+        List<Wood> wood = getEntities(Wood.class);
+        List<Arrow> arrows = getEntities(Arrow.class);
+        if ((wood.size() >= 1 && arrows.size() >= 3 && !forceShield)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean midnightArmourCanBeBuilt() {
+        List<SunStone> sunStone = getEntities(SunStone.class);
+        List<Sword> sword = getEntities(Sword.class);
+        if (sword.size() >= 1 && sunStone.size() >= 1) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean sceptreCanBeBuilt() {
+        List<Wood> wood = getEntities(Wood.class);
+        List<Arrow> arrows = getEntities(Arrow.class);
+        List<Treasure> treasure = getEntities(Treasure.class);
+        List<Key> keys = getEntities(Key.class);
+        List<SunStone> sunStone = getEntities(SunStone.class);
+
+        if ((wood.size() >= 1 || arrows.size() >= 2) && (keys.size() >= 1 || treasure.size() >= 1)
+                && sunStone.size() >= 1) {
+            return true;
+        }
+        return false;
     }
 
     public <T extends InventoryItem> T getFirst(Class<T> itemType) {
